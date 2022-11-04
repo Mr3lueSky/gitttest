b440abf80197ff42f50e15b825cf527a5b9bf5a5
Jyothish Tr <trjyothish0@gmail.com>
2022-11-05 01:55:35 +0530

    New batman endpoint

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('hello world'));
 app.get('/hello', (req, res) => res.send('sup boii!!'));
+app.get('/batman', (req, res) => res.send('I am batman'));
 
 app.listen(process.env.PORT || 5000);
