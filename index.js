6fb2b873f3fd000ed0e046d26681b3d91f95521b
Naveen K R <thenaveenkr7@gmail.com>
2022-11-05 01:44:39 +0530

    hello endpoint

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('hello world'));
+app.get('/hello', (req, res) => res.send('sup boii!!'));
 
 app.listen(process.env.PORT || 5000);
